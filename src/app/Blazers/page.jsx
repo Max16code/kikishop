6c8961869b5eff2486273a5c72305e35586042da
max16code <amanzeikedi16@gmail.com>
2025-07-03 17:28:16 +0100

    kikione

---
+++ src/app/Blazers/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+import Navbar from '@/components/Navbar'
+
+export default function page() {
+  return (
+    <div>
+      <h1>blazer</h1>
+    </div>
+  )
+}
