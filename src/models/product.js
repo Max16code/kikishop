45c26b904f6d8e65086196ec4431f03fe040dc20
max16code <amanzeikedi16@gmail.com>
2025-07-06 14:18:08 +0100

    nicetouch

--- src/models/product.js
+++ src/models/product.js
@@ -1,18 +1,21 @@
 // models/Product.js
-
 import mongoose from 'mongoose';
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
+const productSchema = new mongoose.Schema({
+  title: String,
   description: String,
-  image: String,
   price: Number,
-  sizes: [String],
-  category: String, // E.g., "Blazers", "Dresses"
+  image: String,
+  size: [String], // e.g. ['S', 'M', 'L']
+  category: {
+    type: String,
+    enum: ['blazers', 'shirts', 'skirts', 'jeans', 'gym', 'dresses'],
+    required: true
+  },
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
-export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
+export default mongoose.models.Product || mongoose.model('Product', productSchema);
